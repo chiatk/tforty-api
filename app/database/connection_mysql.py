cedec8cab9181902b3f46e8716161f89b6e50a20
liquid psy <omar.psytrance9@gmail.com>
2021-08-29 11:14:12 -0500

    Uso de dotenv en master

--- app/database/connection_mysql.py
+++ app/database/connection_mysql.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 host        =   os.environ['host']
 database    =   os.environ['database']
